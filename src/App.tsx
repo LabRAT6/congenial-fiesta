401e942c481c3702dc4dbfb7238b7825f2b27694
Martin Lõõbas <94774558+LabRAT6@users.noreply.github.com>
2025-08-15 15:23:40 +0300

    Update App.tsx
    ui authenticator

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { useEffect, useState } from "react";
+import { useAuthenticator } from '@aws-amplify/ui-react';
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 
 const client = generateClient<Schema>();
 
 function App() {
+    const { signOut } = useAuthenticator();
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
     });
+          <button onClick={signOut}>Sign out</button>
   }, []);
 
   function createTodo() {
