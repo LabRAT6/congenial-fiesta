394918afff93617b65dabf0bdef8b22460b4ffdf
Martin Lõõbas <94774558+LabRAT6@users.noreply.github.com>
2025-08-15 16:30:28 +0300

    Update main.tsx
    ui identificator

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Authenticator } from '@aws-amplify/ui-react';
+import { Amplify } from 'aws-amplify';
 import App from "./App.tsx";
+import outputs from '../amplify_outputs.json';
 import "./index.css";
-import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
 import '@aws-amplify/ui-react/styles.css';
+
 Amplify.configure(outputs);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+ <React.StrictMode>
+    <Authenticator>
+      <App />
+    </Authenticator  
+  <React.StrictMode>
+    <Authenticator>
+      <App />
+    </Authenticator>
+  </React.StrictMode>>
+  </React.StrictMode>
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
